63f8b7b5b6d78067ded1434734f352bd662b91eb
Antonio Bandeira <bandeiramagalhaesoficial@gmail.com>
2022-09-14 11:03:10 -0300

    Feita a atividade do carro.

--- Uma Classe/Carro/main.cpp
+++ Uma Classe/Carro/main.cpp
@@ -3,33 +3,63 @@
 #include <aux.hpp>
 
 
-class Car {
-public:
-    int pass;
-    int passMax;
-    int gas;
-    int gasMax;
-    int km;
+struct Car {
 
-    Car() { // todo
+    int pass {0};
+    int passMax {2};
+    int gas {0};
+    int gasMax {100};
+    int km {0};
+
+    Car() {
     }
 
-    void enter() { // todo
+    void enter() { 
+        if (pass == passMax)
+            std::cout << "fail: limite de pessoas atingido\n";
+        else
+            pass++;
     }
 
-    void leave() { // todo
+    void leave() { 
+        if (pass == 0) 
+            std::cout << "fail: nao ha ninguem no carro\n";
+        else
+            pass--;
     }
 
-    void fuel(int gas) { // todo
+    void fuel(int gas) { 
+        if (gas <= gasMax)   
+            this->gas += gas;
+        else
+            this->gas = gas - gasMax;
     }
 
-    void drive(int km) { // todo
-    }
+    void drive(int km){   
+        if (pass == 0) {
+            std::cout << "fail: nao ha ninguem no carro\n";
+        } else if (pass >= 1 && gas >= km)  {
+            gas -= km;
+            this->km += km;
+        } else if (gas == 0 && km > gas) {
+            std::cout << "fail: tanque vazio\n";
+        } else if (pass >= 1 && km > gas) {
+            if (km > gasMax) {
+                gas -= 100;
+                this->km += 100;
+            }
+
+            std::cout << "fail: tanque vazio apos andar " << gas << " km\n"; 
+            this->km += gas;
+            gas = 0;
+        }
+}
 
     std::string str() {
-        return {}; // todo
-       std::stringstream ss;
-       ss << "pass: " << pass << ", gas: " << gas << ", km: " << km << "";
+        // return {};
+        std::stringstream ss;
+        ss << "pass: " << pass << ", gas: " << gas << ", km: " << km << "";
+        return ss.str();
     }
 };
 
