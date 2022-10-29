f694fd1ccb3f53128bb904976b9de9c8aef2b5ce
Antonio Bandeira <bandeiramagalhaesoficial@gmail.com>
2022-10-28 22:26:22 -0300

    up

--- ArrayList I - Listas/Pula Pula/main.cpp
+++ ArrayList I - Listas/Pula Pula/main.cpp
@@ -6,47 +6,57 @@
 
 using namespace aux;
 
-class Kid {
+class Kid
+{
 private:
     int age;
     std::string name;
 
 public:
-    Kid(std::string name, int age): age(age), name(name)
-    {}
+    Kid(std::string name, int age) : age(age), name(name)
+    {
+    }
 
-    int getAge() {
+    int getAge()
+    {
         return age;
     }
-    
-    std::string getName() {
+
+    std::string getName()
+    {
         return name;
     }
 
-    std::string str() {
+    std::string str()
+    {
         return name + ":" + std::to_string(age);
     }
 };
 
 using PtrKid = std::shared_ptr<Kid>;
 
-std::ostream& operator<<(std::ostream& os,  PtrKid kid) {
+std::ostream &operator<<(std::ostream &os, PtrKid kid)
+{
     return (os << kid->str());
 }
 
-class Trampoline {
+class Trampoline
+{
     std::list<PtrKid> waiting;
     std::list<PtrKid> playing;
-    
-    PtrKid removeFromList(std::string name, std::list<PtrKid>& lista) {
-        for(auto it = lista.begin(); it != lista.end();it++) {
+
+    PtrKid removeFromList(std::string name, std::list<PtrKid> &lista)
+    {
+        for (auto it = lista.begin(); it != lista.end(); it++)
+        {
             auto kid = *it;
-            if(kid->getName() == name) {
+            if (kid->getName() == name)
+            {
                 lista.erase(it);
                 return kid;
             }
         }
-        return {};        
+        return {};
         // for (auto kidSearch : lista) {
         //     if (kidSearch == name->getName()) {
         //         auto kid = kidSearch;
@@ -57,7 +67,7 @@ class Trampoline {
         //         return{};
         //     }
         // }
-        
+
         // auto it = std::find(lista.begin(), lista.end(), name);
 
         // if (kid == lista.end()) {
@@ -72,27 +82,33 @@ class Trampoline {
     }
 
 public:
-    Trampoline() {
+    Trampoline()
+    {
     }
-    
-    void arrive(PtrKid kid) {
+
+    void arrive(PtrKid kid)
+    {
         waiting.push_front(kid);
     }
 
-    void enter() {
-        if (waiting.empty()) {
+    void enter()
+    {
+        if (waiting.empty())
+        {
             // std::cout << "fail: lista de espera está vazia!\n";
             return;
         }
-        
+
         auto it = waiting.back();
         waiting.pop_back();
 
         playing.push_front(it);
     }
 
-    void leave() {
-        if (playing.empty()) {
+    void leave()
+    {
+        if (playing.empty())
+        {
             // std::cout << "fail: lista de\n";
             return;
         }
@@ -102,45 +118,61 @@ public:
         return;
     }
 
-    PtrKid removeKid(std::string name) {
+    PtrKid removeKid(std::string name)
+    {
         return (removeFromList(name, waiting), removeFromList(name, playing));
     }
 
-    std::string str() {
+    std::string str()
+    {
         std::stringstream ss;
         std::string exitOne, exitTwo;
 
-        for (auto kid : waiting) {
-            if (kid == waiting.back()) {
+        for (auto kid : waiting)
+        {
+            if (kid == waiting.back())
+            {
                 exitOne += kid->str();
-            } else {
+            }
+            else
+            {
                 exitOne += kid->str() + ", ";
             }
         }
-        for (auto kid : playing) {
-            if (kid == playing.back()) {
+        for (auto kid : playing)
+        {
+            if (kid == playing.back())
+            {
                 exitTwo += kid->str();
-            } else {
+            }
+            else
+            {
                 exitTwo += kid->str() + ", ";
             }
         }
-        
-        ss << "[" << exitOne << "] => [" 
-            << exitTwo << "]";
-        
+
+        ss << "[" << exitOne << "] => ["
+           << exitTwo << "]";
+
         return ss.str();
     }
 };
 
-int main() {
+int main()
+{
     aux::Chain chain;
     aux::Param param;
     Trampoline tr;
-    chain["arrive"] = [&]() { tr.arrive(std::make_shared<Kid>(param[1], aux::to<int>(param[2]))); };
-    chain["enter"]  = [&]() { tr.enter(); };
-    chain["leave"]  = [&]() { tr.leave(); };
-    chain["remove"] = [&]() { tr.removeKid(param[1]); };
-    chain["show"]   = [&]() { std::cout << tr.str() << std::endl; };
+    chain["arrive"] = [&]()
+    { tr.arrive(std::make_shared<Kid>(param[1], aux::to<int>(param[2]))); };
+    chain["enter"] = [&]()
+    { tr.enter(); };
+    chain["leave"] = [&]()
+    { tr.leave(); };
+    chain["remove"] = [&]()
+    { tr.removeKid(param[1]); };
+    chain["show"] = [&]()
+    { std::cout << tr.str() << std::endl; };
 
     aux::execute(chain, param);
 }
